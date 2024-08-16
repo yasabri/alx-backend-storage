294a6b3f7bb07f4f0c9cd82487fb43bf53843cc4
yasabri <yasabri04@gmail.com>
2024-08-16 11:50:02 -0700

    me

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 10's module'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of collection's document based on name'''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 9's module'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts a new document in collection'''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
